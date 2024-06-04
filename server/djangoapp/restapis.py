d8cf2fe3f313d23d8b7d420dbbc2c6589b19937f
Bin Lama <binlama@yahoo.com>
2024-06-04 15:26:27 -0700

    linter compliance

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -11,12 +11,13 @@ sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
 
+
 def get_request(endpoint, **kwargs):
     params = ""
     if (kwargs):
         for key, value in kwargs.items():
             params = params+key+"="+value+"&"
-    
+
     request_url = backend_url+endpoint+"?"+params
 
     print(f"GET from {request_url}")
@@ -32,6 +33,8 @@ def get_request(endpoint, **kwargs):
 # def analyze_review_sentiments(text):
 # request_url = sentiment_analyzer_url+"analyze/"+text
 # Add code for retrieving sentiments
+
+
 def analyze_review_sentiments(text):
     request_url = sentiment_analyzer_url+"analyze/"+text
     try:
@@ -43,6 +46,8 @@ def analyze_review_sentiments(text):
         print("Network exception occurred")
 
 # Add code for posting review
+
+
 def post_review(data_dict):
     request_url = backend_url+"/insert_review"
     try:
